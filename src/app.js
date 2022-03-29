c290f839efa9dffecb671515adb844067324d893
MaximVovk <maksemvovk@gmail.com>
2022-03-29 18:43:01 +0300

    add test commit for check github action correct work

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/test', (_req, res) =>  {
-  res.status(200).send('Hello world')
+  res.status(200).send('Hello world, test commit')
 })
 
 module.exports = app
